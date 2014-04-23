61f4411ac06c723185bf84f2f14e4e92bd9d0dc3
Mirza Baig <mirzaq4@gmail.com>
2014-04-23 09:13:55 -0400

    challenge 12

--- Coderbyte Challenges/Medium/012 SimpleMode/SimpleMode.php
+++ Coderbyte Challenges/Medium/012 SimpleMode/SimpleMode.php
@@ -5,12 +5,13 @@ Developer: Mirza S. Baig
 
 Date: 4-23-2014
 
-Description:    Using the PHP language, have the function MeanMode(arr) take the array
-                of numbers stored in arr and return 1 if the mode equals the mean, 0 if
-                they don't equal each other (ie. [5, 3, 3, 3, 1] should return 1
-                because the mode (3) equals the mean (3)). The array will not be
-                empty, will only contain positive integers, and will not contain more
-                than one mode.
+Description:    Using the PHP language, have the function SimpleMode(arr) take the
+                array of numbers stored in arr and return the number that appears most
+                frequently (the mode). For example: if arr contains [10, 4, 5, 2, 4]
+                the output should be 4. If there is more than one mode return the one
+                that appeared in the array first (ie. [5, 10, 10, 6, 5] should return
+                5 because it appeared first). If there is no mode return -1. The array
+                will not be empty.
 
 */
 
